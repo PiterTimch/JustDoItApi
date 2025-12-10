ec137a58ef9269a52f7f0ada0e231e9442267dea
Petro <petrotymchuck@gmail.com>
2025-12-10 10:29:07 +0200

    add image service

--- JustDoItApi/Controllers/ZadachiController.cs
+++ JustDoItApi/Controllers/ZadachiController.cs
@@ -12,9 +12,9 @@ namespace JustDoItApi.Controllers
         public IActionResult Get()
         {
             var items = new List<ZadachaItemModel> { 
-                new ZadachaItemModel() { Id = 1, Name = "ѕисати код" },
-                new ZadachaItemModel() { Id = 2, Name = "ѕисати б≥льше коду!" },
-                new ZadachaItemModel() { Id = 3, Name = "ѕисати ще б≥льше коду!!!" }
+                new ZadachaItemModel() { Id = 1, Name = "ѕисати код", Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Dog_Breeds.jpg/1024px-Dog_Breeds.jpg" },
+                new ZadachaItemModel() { Id = 2, Name = "ѕисати б≥льше коду!", Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Dog_Breeds.jpg/1024px-Dog_Breeds.jpg" },
+                new ZadachaItemModel() { Id = 3, Name = "ѕисати ще б≥льше коду!!!", Image = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/Dog_Breeds.jpg/1024px-Dog_Breeds.jpg" }
             };
 
             return Ok(items);
--- JustDoItApi/Models/Zadachi/ZadachaItemModel.cs
+++ JustDoItApi/Models/Zadachi/ZadachaItemModel.cs
@@ -4,4 +4,5 @@ public class ZadachaItemModel
 {
     public int Id { get; set; }
     public string Name { get; set; } = string.Empty;
+    public string Image { get; set; } = string.Empty;
 }
