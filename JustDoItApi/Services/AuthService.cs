6f6ad245f82a5b44cf432f4db9e3e56d3a20e722
Petro <petrotymchuck@gmail.com>
2025-12-17 08:06:10 +0200

    add user structure

---
+++ JustDoItApi/Services/AuthService.cs
@@ -0,0 +1,44 @@
+﻿using AutoMapper;
+using JustDoItApi.Entities.Identity;
+using JustDoItApi.Interfaces;
+using JustDoItApi.Models.Auth;
+using Microsoft.AspNetCore.Identity;
+
+namespace JustDoItApi.Services;
+
+public class AuthService(IJWTTokenService tokenService,
+        UserManager<UserEntity> userManager,
+        IMapper mapper,
+        IImageService imageService) : IAuthService
+{
+    public async Task<string> LoginAsync(LoginModel model)
+    {
+        var user = await userManager.FindByEmailAsync(model.Email);
+        if (user != null && await userManager.CheckPasswordAsync(user, model.Password))
+        {
+            var token = await tokenService.CreateTokenAsync(user);
+            user.IsDeleted = false;
+            await userManager.UpdateAsync(user);
+            return token;
+        }
+        return string.Empty;
+    }
+
+    public async Task<string> RegisterAsync(RegisterModel model)
+    {
+        var user = mapper.Map<UserEntity>(model);
+        if (model.ImageFile != null)
+        {
+            user.Image = await imageService.SaveImageAsync(model.ImageFile);
+        }
+        var result = await userManager.CreateAsync(user, model.Password);
+
+        if (result.Succeeded)
+        {
+            await userManager.AddToRoleAsync(user, "User");
+            var token = await tokenService.CreateTokenAsync(user);
+            return token;
+        }
+        return string.Empty;
+    }
+}
