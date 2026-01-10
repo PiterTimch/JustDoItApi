9144b3774f566205f77f79e19cff65458451894b
Petro <petrotymchuck@gmail.com>
2026-01-10 18:44:24 +0200

    add xaml example

--- JustDoItApi/Models/Auth/LoginModel.cs
+++ JustDoItApi/Models/Auth/LoginModel.cs
@@ -1,8 +1,24 @@
 ﻿namespace JustDoItApi.Models.Auth;
 
+/// <summary>
+/// Json - модель для входу користувача
+/// </summary>
 public class LoginModel
 {
+    /// <summary>
+    /// Email
+    /// </summary>
+    /// <example>
+    /// admin@example.com
+    /// </example>
     public string Email { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Password
+    /// </summary>
+    /// <example>
+    /// Admin123!
+    /// </example>
 
     public string Password { get; set; } = string.Empty;
 }
