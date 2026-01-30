a1a4bb8d4b22a40193b0ae4b16d8115ec7f0f487
Petro <petrotymchuck@gmail.com>
2026-01-30 07:06:50 +0200

    add base chat tbl structure

---
+++ JustDoItApi/Entities/Chat/ChatMessageReadEntity.cs
@@ -0,0 +1,17 @@
+﻿using JustDoItApi.Entities.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JustDoItApi.Entities.Chat;
+
+[Table("tbl_chat_message_reads")]
+public class ChatMessageReadEntity
+{
+    public long MessageId { get; set; }
+    public ChatMessageEntity? Message { get; set; }
+
+    public long UserId { get; set; }
+    public UserEntity? User { get; set; }
+
+    public DateTime ReadAt { get; set; } = DateTime.UtcNow;
+}
+
---
+++ JustDoItApi/Entities/Chat/ChatUserEntity.cs
@@ -0,0 +1,18 @@
+﻿using JustDoItApi.Entities.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JustDoItApi.Entities.Chat;
+
+[Table("tbl_chat_users")]
+public class ChatUserEntity
+{
+    public long ChatId { get; set; }
+    public ChatEntity? Chat { get; set; }
+
+    public long UserId { get; set; }
+    public UserEntity? User { get; set; }
+
+    public bool IsAdmin { get; set; } = false;
+    public DateTime JoinedAt { get; set; } = DateTime.UtcNow;
+}
+
